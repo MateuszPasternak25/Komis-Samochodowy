8f0f675158358cc00a038c51cf373d488c4c1418
MateuszPasternak25 <168337466+MateuszPasternak25@users.noreply.github.com>
2024-06-21 14:35:09 +0200

    Create AddSamochodForm.cs

---
+++ AddSamochodForm.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class AddSamochodForm : Form
+    {
+        private Zarzadzanie zarzadzanie;
+
+        public AddSamochodForm(Zarzadzanie zarzadzanie)
+        {
+            InitializeComponent();
+            this.zarzadzanie = zarzadzanie;
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            string marka = txtMarka.Text;
+            string model = txtModel.Text;
+            int rocznik = int.Parse(txtRocznik.Text);
+            int przebieg = int.Parse(txtPrzebieg.Text);
+            decimal cena = decimal.Parse(txtCena.Text);
+            bool czySprzedany = chkCzySprzedany.Checked;
+
+            zarzadzanie.DodajSamochod(marka, model, rocznik, przebieg, cena, czySprzedany);
+            this.Close();
+        }
+
+        private void AddSamochodForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
